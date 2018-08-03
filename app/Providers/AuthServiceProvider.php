796a4005244b5a0385dbd1a73b56f257c46b9e37
abanob todary <abanob.todary@gmail.com>
2018-08-03 15:31:29 +0200

    Update AuthServiceProvider.php

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -28,7 +28,7 @@ class AuthServiceProvider extends ServiceProvider
 
         Passport::routes();
 
-        Passport::tokensExpireIn(now()->addDays(15));
+        Passport::tokensExpireIn(now()->addDays(1));
         //
     }
 }
